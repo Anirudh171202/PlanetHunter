ef8137e9cdc899d94020ff1d08d01be81545a6ff
Anirudh171202 <anirudh.sathiya@gmail.com>
2020-03-20 21:01:57 +0530

    removed gauss, add2mainp

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import warnings
 import math
 from reduce_memory import reducememory
 from plot_graphs import flux_dist
+from plot_graphs import exoplanets, non_exoplanets
 
 import numpy as np 
 import sklearn as sk
@@ -17,7 +18,7 @@ warnings.filterwarnings('ignore')
 train_data = pnds.read_csv('exoTrain.csv').fillna(0)
 test_data = pnds.read_csv('exoTest.csv').fillna(0)
 
-flux_dist(train_data)
+#flux_dist(train_data)
 
 x_train = train_data.drop(["LABEL"], axis = 1)
 y_train = train_data["LABEL"]
@@ -25,7 +26,10 @@ y_train = train_data["LABEL"]
 x_test = test_data.drop(["LABEL"], axis = 1)
 y_test = test_data["LABEL"]
 
+exoplanets(x_train)
+#exoplanets(x_test)
 
+non_exoplanets(x_train)
 
 
 
--- plot_graphs.py
+++ plot_graphs.py
@@ -11,4 +11,17 @@ def flux_dist(df):
 	plt.plot(df.iloc[2,])
 	plt.show()
 
-
+def non_exoplanets(df):
+    labels_1=[100,200,300]
+    for i in labels_1:
+        plt.hist(df.iloc[i,:], bins=200)
+        plt.title("Gaussian Histogram for non_exoplanets")
+        plt.xlabel("Flux values")
+        plt.show()
+def exoplanets(df):
+    labels_1=[16,21,25]
+    for i in labels_1:
+        plt.hist(df.iloc[i,:], bins=200)
+        plt.title("Gaussian Histogram for exoplanets")
+        plt.xlabel("Flux values")
+        plt.show()
